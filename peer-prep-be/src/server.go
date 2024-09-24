2aac7ddf0df4b57dad1d8dfe660fc1f7e95287cc
lynnetteeee <lynnette.oxh@gmail.com>
2024-09-24 14:19:33 +0800

    Create get and post requests for questions

--- peer-prep-be/src/server.go
+++ peer-prep-be/src/server.go
@@ -11,7 +11,11 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/CS3219-AY2425S1/cs3219-ay2425s1-project-g01/peer-prep-be/models"
 )
+
+var questionCollection *mongo.Collection
 
 func main() {
 	key := "MONGODB_URI"
@@ -43,9 +47,63 @@ func main() {
 	}
 	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 
+	questionCollection = client.Database("peerprep").Collection("questions")
+
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.POST("/questions", createQuestion)
+	e.GET("/questions", getQuestions)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func createQuestion(c echo.Context) error {
+	fmt.Println("Received POST /questions request")
+	var question models.Question
+
+    if err := c.Bind(&question); err != nil {
+        return c.JSON(http.StatusBadRequest, echo.Map{"error": "Invalid input"})
+    }
+
+    // Generate a new ObjectID
+    question.ID = primitive.NewObjectID()
+
+    _, err := questionCollection.InsertOne(context.TODO(), question)
+    if err != nil {
+        return c.JSON(http.StatusInternalServerError, echo.Map{"error": "Failed to create question"})
+    }
+
+	fmt.Println("Successfully inserted question")
+    return c.JSON(http.StatusCreated, question)
+
+}
+
+func getQuestions(c echo.Context) error {
+	fmt.Println("Received GET /questions request")
+    var questions []models.Question
+
+    cursor, err := questionCollection.Find(context.TODO(), bson.M{})
+    if err != nil {
+        fmt.Println("Failed to retrieve questions")
+        return c.JSON(http.StatusInternalServerError, echo.Map{"error": "Failed to retrieve questions"})
+    }
+    defer cursor.Close(context.TODO())
+
+    for cursor.Next(context.TODO()) {
+        var question models.Question
+        cursor.Decode(&question)
+        questions = append(questions, question)
+    }
+
+    if err := cursor.Err(); err != nil {
+        fmt.Println("Error while decoding")
+        return c.JSON(http.StatusInternalServerError, echo.Map{"error": "Error while decoding"})
+    }
+
+    fmt.Println("Successfully retrieved questions")
+    return c.JSON(http.StatusOK, questions)
 }
