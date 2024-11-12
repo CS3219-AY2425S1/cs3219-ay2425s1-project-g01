a0e1d25601b7889609f9727c525a771ed6b501c9
lynnetteeee <lynnette.oxh@gmail.com>
2024-11-13 03:24:41 +0800

    Merge nonsense

---
+++ peer-prep-fe/src/services/code-websocket.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebSocketService {
+  private socket$!: WebSocketSubject<any>;
+
+  constructor() {}
+
+  isProduction(): boolean {
+    return window.location.hostname !== 'localhost';
+  }
+
+  connect(sessionId: string, userId: string): void {
+    if (this.socket$) {
+      this.disconnect();
+    }
+
+    this.socket$ = this.isProduction() 
+    ? webSocket(`wss://peer-prep-gateway-1093398872288.asia-southeast1.run.app/websocket/${sessionId}?userID=${userId}`)
+    : webSocket(`ws://localhost:8081/${sessionId}?userID=${userId}`);
+
+  }
+
+  sendMessage(message: any): void {
+    this.socket$.next(message);
+  }
+
+  getMessages(): Observable<any> {
+    return this.socket$.asObservable();
+  }
+
+  disconnect(): void {
+    if (this.socket$) {
+      this.socket$.complete();
+    }
+  }
+}
